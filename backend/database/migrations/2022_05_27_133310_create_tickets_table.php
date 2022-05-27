f124437ec987ee3d71fa7fa401178b5d02ad17df
Bartomiej Zocki <b.zlocki@bonasoft.pl>
2022-05-27 23:22:13 +0200

    fixed tickets migration

--- backend/database/migrations/2022_05_27_133310_create_tickets_table.php
+++ backend/database/migrations/2022_05_27_133310_create_tickets_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
-            // $table->bigIncrements('id');
-            $table->integer('screening_id');
-            $table->integer('seat_id');
-            $table->integer('user_id');
-            $table->primary(['screening_id', 'seat_id', 'user_id']);
+            $table->id('id');
+            $table->unsignedBigInteger('seat_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('screening_id');
+            $table->foreign('screening_id')->references('id')->on('screenings');
+            $table->foreign('seat_id')->references('id')->on('seats');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
