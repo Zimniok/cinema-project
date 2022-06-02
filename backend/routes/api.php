0fde428fed42278c2d4575c45e075db93d687815
Bartomiej Zocki <b.zlocki@bonasoft.pl>
2022-06-02 23:54:29 +0200

    connecting with vue

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
