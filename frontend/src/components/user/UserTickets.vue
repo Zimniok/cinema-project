47a7b3f9eff0df1422974a9a2ea84a04ca8895b5
Bartomiej Zocki <b.zlocki@bonasoft.pl>
2022-06-03 03:55:09 +0200

    ticket table

---
+++ frontend/src/components/user/UserTickets.vue
@@ -0,0 +1,16 @@
+
+<template>
+	<table class="table">
+		<thead>
+			<th scope="col">ID</th>
+			<th scope="col">Movie</th>
+			<th scope="col">Time</th>
+			<th scope="col">Seat row</th>
+			<th scope="col">Seat number</th>
+		</thead>
+		<tbody>
+			
+		</tbody>
+	</table>
+
+</template>
